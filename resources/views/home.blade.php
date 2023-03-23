04734bd62f1b9f4339ec8ff152a5bc154940bda6
OBrien-reece <indeche.evans.o@gmail.com>
2023-03-23 14:44:25 +0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 m-auto">
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">Tech and Gen</h1>
+                    <p class="lead">
+                        <small><span><i>Two arrays walk into a bar...</i></span></small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
