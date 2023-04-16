d2f87597b6b8558cc6ba76aaf480b0eca8ba0b60
OBrien-reece <indeche.evans.o@gmail.com>
2023-04-16 11:25:57 +0300

    included slug in user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,7 +8,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Psy\Util\Str;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -69,6 +69,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'slug' => Str::slug($data['name'], '_'),
         ]);
     }
 }
+
