b86b585df0b2ff9e58f154998b57f6c7873e40d0
OBrien-reece <indeche.evans.o@gmail.com>
2023-04-21 14:25:45 +0300

    Created article creation form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,6 +98,24 @@
 
 {{--Become a writer modal logic ends--}}
 
+    <script type="text/javascript">
+        // Auto-Grow-TextArea script.
+        // Script copyright (C) 2011 www.cryer.co.uk.
+        // Script is free to use provided this copyright header is included.
+        function AutoGrowTextArea(textField)
+        {
+            if (textField.clientHeight < textField.scrollHeight)
+            {
+                textField.style.height = textField.scrollHeight + "px";
+                if (textField.clientHeight < textField.scrollHeight)
+                {
+                    textField.style.height =
+                        (textField.scrollHeight * 2 - textField.clientHeight) + "px";
+                }
+            }
+        }
+    </script>
+
 </body>
 
 </html>
